c1ef1a2c2f3bc39698308067b4c1376c15c30f67
Ingars Paeglitis <ingars.paeglitis@yahoo.com>
2019-11-15 22:02:24 +0200

    Updates

---
+++ precache-manifest.bb5d94f3efffe4062a63e96e525b0fdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7026bd237f8c85798a4c88abacddbaa2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "575e0c5b07268879219f",
+    "url": "/monsters-rolodex/static/css/main.89a98778.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "575e0c5b07268879219f",
+    "url": "/monsters-rolodex/static/js/main.ba1522f5.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
